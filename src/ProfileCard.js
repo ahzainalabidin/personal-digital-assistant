fa154b6123dc8dc2e1d118307b61692fa802777e
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-08 14:34:25 +0800

    Destructuring props

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,14 @@
 function ProfileCard(props) {
+
+    const { title, handle } = props;
+
     return (
         <div>
-            <div>Title is {props.title}</div>
-            <div>Handle is {props.handle}</div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
         </div>
     );
+
 }
 
 export default ProfileCard;
