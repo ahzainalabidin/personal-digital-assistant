4a4de82e130e74d338362fd81f97ceeecff6bc45
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-08 12:24:45 +0800

    Create reusable component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import ProfileCard from "./ProfileCard";
+
 function App() {
   return (
-    <div></div>
+
+    <div>
+
+      <div>Personal Digital Assistants</div>
+
+      <ProfileCard title="Alexa" handle="@alexa99" />
+      <ProfileCard title="Cortana" handle="@cortana32" />
+      <ProfileCard title="Siri" handle="@siri01" />
+
+    </div>
+
   );
 }
 
