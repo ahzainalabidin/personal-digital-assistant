84a19bde8c5310956d0d4b79ae45df27ddff2e65
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-11 09:07:38 +0800

    Add styling to ProfileCard

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,11 +1,24 @@
 function ProfileCard({ title, handle, image }) {
 
     return (
-        <div>.
-            <img src={image} alt="pda logo" />
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+
+        <div className="card">
+
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="pda logo" />
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+            </div>
+
         </div>
+
     );
 
 }
